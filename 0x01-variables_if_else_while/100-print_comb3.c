2ca8f9f9c6f081cb4cffd2b9cb533f6fc4dc344d
chunaj17 <chunaj300@gmail.com>
2022-12-11 14:24:25 -0800

    all

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,15 +19,13 @@ int main(void)
 	while (i <= 8)
 	{
 		int j = i + 1;
+		
 		while (j < 10)
 		{
 			putchar(i % 10 + '0');
 			putchar(j % 10 + '0');
-			//if (i != 8 && j != 9) 
-			//{
 			putchar(',');
 			putchar(' ');
-			//}
 			j++;
 		}
 		i++;
