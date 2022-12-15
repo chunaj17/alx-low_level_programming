553c33b053a9b2e23b39cc8037b4318012d42392
chunaj17 <chunaj300@gmail.com>
2022-12-14 19:29:35 -0800

    void print_alphabet_x10(void)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Author: Samson Jisso
+ *
+ */
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -> prints 10 times the alphabet
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 10;
+
+	while (i <= 10)
+	{
+		char j = 'a';
+
+		while (j <= 'z')
+		{
+			_putchar(i);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
