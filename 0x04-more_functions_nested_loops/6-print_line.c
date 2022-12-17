b39c5881a85302ad46dd603f9018755490f26170
chunaj17 <chunaj300@gmail.com>
2022-12-16 17:44:55 -0800

    completed

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+/*
+ * File: 6-print_line.c
+ * auth: samson jisso
+ *
+ */
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: integer value to be printed
+ *
+ * Return: noting
+ */
+
+void print_line(int n)
+{
+	int i;
+	
+	if (n != 0 && n >= 0)
+	{
+	for(i = 0; i <= n - 1; i++)
+	{
+		_putchar('_');
+	}
+	}
+	_putchar('\n');
+}
