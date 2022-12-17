3b4d3d2ce5f62105b9bef108fea037c8408564d7
chunaj17 <chunaj300@gmail.com>
2022-12-16 16:36:41 -0800

    completed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * File - 3-print_numbers.c
+ * Auth - SJ
+ *
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ *
+ * Return: noting
+ */
+
+void print_numbers(void)
+{
+	for (int i = 48; i <= 57; i++)
+	{
+		_putchar(i)
+	}
+}
