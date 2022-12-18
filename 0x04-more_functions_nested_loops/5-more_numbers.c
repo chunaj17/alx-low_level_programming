ea50a1a6cb9fa51a62610756748b6df6aec1f508
chunaj17 <chunaj300@gmail.com>
2022-12-18 04:43:04 -0800

    fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,24 +5,36 @@
 #include "main.h"
 
 /**
+ * print_more - prints numbers more than 9 using putchar
+ *
+ */
+void print_more(int n)
+{
+	if (n / 10)
+		print_more(n / 10);
+	_putchar(n % 10 + '0');
+}
+/**
  * more_numbers - prints 10 times the numbers, from 0 to 14.
  *
  * Returns: noting
  */
 void more_numbers(void)
 {
-	int i, j, n;
+	int i, n;
 
-	for (n = 0; n == 10; n++)
+	for (n = 0; n < 10; n++)
 	{
-		for (i = 48; i <= 57; i++)
+		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i);
-		}
-		for (j = 48; j <= 52; j++)
-		{
-			_putchar(49);
-			_putchar(j);
+			if (i <= 9)
+			{
+				_putchar(i + '0');
+			}
+			else 
+			{
+				print_more(i);
+			}
 		}
 		_putchar('\n');
 	}
