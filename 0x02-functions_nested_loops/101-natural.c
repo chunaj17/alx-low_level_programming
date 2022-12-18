a1fecdbd7a84e8bc70a0e76e490c37b9500bb69a
chunaj17 <chunaj300@gmail.com>
2022-12-18 03:08:23 -0800

    completed

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,37 @@
+/*
+ * File: 101-natural.c
+ * auth: samson jisso
+ */
+
+#include <stdio.h>
+/**
+ * multiples - prints all multiples of 3 and 5
+ *
+ * 
+ */
+
+void multiples(void)
+{
+	int i, sum = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if (i / 3 || i / 5 )
+		{
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+	putchar('\n');
+}
+
+/**
+ * main - pirnts the sum of 3 and 5 multiples
+ *
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	multiples();
+	return (0);
+}
