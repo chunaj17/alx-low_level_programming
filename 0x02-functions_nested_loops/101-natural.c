9cffbc3b9f447cc501f1fcdf72aad48ee7d816f4
chunaj17 <chunaj300@gmail.com>
2022-12-18 04:20:53 -0800

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,13 @@
 /**
  * multiples - prints all multiples of 3 and 5
  *
- * 
+ *
  */
 
 void multiples(void)
 {
 	int i, sum = 0;
-	
+
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
