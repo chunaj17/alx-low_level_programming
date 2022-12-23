0624f523f4845bb3b5e24213ad6e333568658840
chunaj17 <chunaj300@gmail.com>
2022-12-22 16:01:58 -0800

    fix

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,16 @@
+/*
+ * File: 7-leet.c
+ * auth: samson jisso
+ *
+ */
+
+/**
+ * leet - encodes a string into 1337
+ * @c: string to be encoded
+ *
+ * Retrun: string
+ */
+char *leet(char *)
+{
+	return ('a');
+}
