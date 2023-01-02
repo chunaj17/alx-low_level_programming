b515e5de89dfd3b37e261448985f74b4a6d8af2d
chunaj17 <chunaj300@gmail.com>
2023-01-02 12:33:29 -0800

    added file with function the fills memory with a constant byte

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,26 +2,13 @@
  * File: 5-rev_string.c
  * Auth: samson jisso
  */
-
+#include <stdio.h>
 /**
  * rev_string - reverses a string
- * @*s: string to be reversed
+ * @s: string to be reversed
  *
  */
 void rev_string(char *s)
 {
-	int i = -1;
-
-	while (*s)
-	{
-		s++;
-		i++;
-	}
-
-	while (i >= 0)
-	{
-		s = i;
-		--i;
-	}
-
+	printf("string %d\n", *s++);
 }
