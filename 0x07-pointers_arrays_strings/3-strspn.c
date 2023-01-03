07f5b0f337e5be07c5767824df070dd6405707b4
chunaj17 <chunaj300@gmail.com>
2023-01-02 17:11:10 -0800

    completed

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: the user string
+ * @accept: the substring to be searched
+ *
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == acccept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+	return (z);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - a function that searches as string for any
+ * of  a set of bytes
+ * @s: the string
+ * @accept: the string searched
+ *
+ * Return: a ponter to the byte in s
+ * that mathces one of the bytes in accept,
+ * or Null if no such byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+		while (*s)
+		{
+			for (i = 0; accept[i]; i++)
+			{
+				if (*s == accept[i])
+					return (s);
+			}
+			s++;
+		}
+	return (NULL);
+}
